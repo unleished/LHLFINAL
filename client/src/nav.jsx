396780d09f3701b10684391c4be06c3da5b0b724
Linh Le Kim <linhlekim@gmail.com>
2018-09-24 18:04:05 +0000

    fixed css on nav bar

--- client/src/nav.jsx
+++ client/src/nav.jsx
@@ -17,14 +17,20 @@ class Nav extends Component {
 
   render() {
     return (
-      <nav id="nav-bar">
+      <nav id="nav-bar" className="navbar navbar-expand-lg navbar-light bg-light">
         <img className="logo" src="images/tryitlogo.png"/>
-        <div id="nav-text">
-          <span className="header">Vyo͞o</span>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/products">Product</NavLink></li>
-        </div>
+        <span className="navbar-brand header" href="#">Vyo͞o</span>
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+             <NavLink to="/"> Home </NavLink>
+            </li>
+            <li className="nav-item">
+             <NavLink to="/products"> Product </NavLink>
+            </li>
+          </ul>
+
       </nav>
+
     );
   }
 }
