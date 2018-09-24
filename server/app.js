32d00b382616ca643b7116eb3fbc8b79a2df4aa7
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-09-24 19:13:24 +0000

    adding heroku start and setting up client build folder

--- server/app.js
+++ server/app.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors());
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
